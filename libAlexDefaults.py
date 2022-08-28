495fa12ec6da85c61705d7f1516ff69ea1c3a21c
Brody Childs <maximombro@gmail.com>
2022-08-28 14:50:02 -0400

    Added initial load function for Lib Alex items

---
+++ libAlexDefaults.py
@@ -0,0 +1,69 @@
+# LibAlexandria: Defaults
+# The default values for various components of the Python 3 bindings.
+
+# Imports
+import os
+import re
+from unicodedata import normalize
+
+# Variables
+VER_LIBALEX = "2.0.0"
+
+DEFAULT_ITEM_DIRECTORY = None
+DEFAULT_ITEM_METAFILEPATH = None
+DEFAULT_ITEM_RESOLVEDFLAGS = None
+
+DEFAULT_CLASSIFICATION = None
+DEFAULT_TITLE = "Untitled"
+DEFAULT_AUTHOR = "Anonymous"
+DEFAULT_DATE = "Undated" # NOTE: This is normally a year in 'YYYY-MM-DD' format.
+DEFAULT_SOURCEFILE = None
+DEFAULT_OTHERFILES = None
+DEFAULT_FLAGS = None
+DEFAULT_DESCRIPTION = "An empty LibAlexandria Item."
+
+# Functions
+def checkPath(path, createPath = False, verbose = False) -> tuple:
+    """
+    Cleans and verifies a string path and normalizes it. Also checks the env for validity.
+
+    path: The string path to clean.
+    createPath: Boolean indicating if the path should be created if it doesn't exist.
+    verbose: If verbose logging should be enabled.
+
+    Returns if the path exists and the full resolved path as a Tuple like (True, "...etc").
+    """
+
+    # Resolve the path of any symbolism
+    path = os.path.expanduser(path)
+    path = os.path.abspath(path)
+
+    # Check if the path exists
+    if not os.path.exists(path):
+        if createPath:
+            # Build the desired path
+            os.makedirs(path)
+        else:
+            # Fail
+            if verbose:
+                print(f"Provided path does not exist: {path}")
+
+            return (False, path)
+
+    # Success
+    return (True, path)
+
+def slugify(s):
+    """
+    Converts the provided string into a slugified version
+    Modified from (Django)[https://github.com/django/django/blob/d3f4c2b95d2a13a5d9bc0e6413dfdbab21388822/django/utils/text.py#L385].
+
+    s: String to slugify.
+
+    Returns the string slugified.
+    """
+    s = str(s)
+    s = normalize("NFKD", s).encode("ascii", "ignore").decode("ascii")
+    s = re.sub(r"[^\w\s-]", "", s.lower())
+    s = re.sub(r"[-\s]+", "-", s).strip("-_")
+    return s
--- libAlexItem.py
+++ libAlexItem.py
@@ -1,11 +1,82 @@
 # LibAlexandria: LibAlexandria Item
 # A standardized LibAlexandria item that can be used in other Python scripts.
 
-## Imports
+# Imports
+import os
+import json
 
-## Classes
-# LibAlexandria Item
+import libAlexDefaults as laShared
+
+# Defaults
+
+# Classes
 class LibAlexItem():
     """
     A LibAlexandria item representing the information for the provided directory's meta file and content.
     """
+    # Constructor
+    def __init__(self, verbose = False):
+        """
+        verbose: If verbose logging should be enabled.
+        """
+        # Assign variables
+        self.isLoaded = False
+        self.directory = laShared.DEFAULT_ITEM_DIRECTORY
+        self.metaFilepath = laShared.DEFAULT_ITEM_METAFILEPATH
+        self.resolvedFlags = laShared.DEFAULT_ITEM_RESOLVEDFLAGS
+        self.isVerbose = verbose
+
+        # Prepare default values
+        self.classification = laShared.DEFAULT_CLASSIFICATION
+        self.title = laShared.DEFAULT_TITLE
+        self.author = laShared.DEFAULT_AUTHOR
+        self.date = laShared.DEFAULT_DATE
+        self.sourceFile = laShared.DEFAULT_SOURCEFILE
+        self.otherFiles = laShared.DEFAULT_OTHERFILES
+        self.flags = laShared.DEFAULT_FLAGS
+        self.description = laShared.DEFAULT_DESCRIPTION
+
+    def loadMeta(self, dirpath: str, metaFilename = "meta.json"):
+        """
+        Loads this LibAlexandria Item from the given directory.
+
+        dirpath: The path to a directory that conforms to Lib Alexandria standard.
+        metaFilename: The filename and extension of the meta file to load within the provided `dirpath`.
+        """
+        # Check the directory path
+        dirIsValid, self.directory = laShared.checkPath(dirpath, verbose=self.isVerbose)
+        if dirIsValid:
+            # Check the meta file path
+            self.metaFilepath = os.path.join(self.directory, metaFilename)
+            if os.path.isfile(self.metaFilepath):
+                # TODO: Resolve additional flags from the directory structure to `resolvedFlags`
+
+                # Read the meta file
+                with open(self.metaFilepath, "r") as metaFile:
+                    # Load the meta json data
+                    # TODO: Add failure message for bad JSON load
+                    metaJson = json.loads(metaFile.read())
+
+                    # Verify the base level of the json
+                    if isinstance(metaJson, dict):
+                        # Assign values
+                        # self.classification = metaJson.get()
+                        pass
+                    else:
+                        # Failed
+                        print(f"\"{self.metaFilepath}\" is not a valid Metadata file.")
+
+                # TODO: Validation check assigned values
+            else:
+                # Failed
+                print(f"\"{self.metaFilepath}\" could not be found within the provided directory: {self.directory}")
+        else:
+            # Failed
+            print(f"Provided directory does not exist: {self.directory}")
+
+    # Private Functions
+    # TODO: Value assignment function
+    # TODO: Value validation function (together with above?)
+
+    # Functions
+    # TODO: Add function to get all flags from `flags`, `resolvedFlags`, and `classification`
